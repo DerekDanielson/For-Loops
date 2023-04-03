391fd30324653c24eb028398058d7fe6c8080c74
Derek <derekdanielson84@gmail.com>
2023-04-03 10:12:12 -0600

    Notes added

---
+++ app.js
@@ -0,0 +1,10 @@
+for ([initialExpression]: [conditionExpression]; [incrementExpression])
+    |statement;|
+
+    // INITIAL EXPRESSION - Initializes a variable/counter
+    // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
+    // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop.  Usually increments the variable
+    // Statement - Code that will be executed each time the loop is run.  To execute a 'block' of code, use the '{}' syntax
+
+
+    
